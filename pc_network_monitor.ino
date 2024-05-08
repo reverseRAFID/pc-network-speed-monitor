cfd8cf1dbb474957b3974aa96ed34144e46498f4
RAFID_KHAN <rafidbd9@gmail.com>
2024-05-08 20:29:48 +0600

    Update pc_network_monitor.ino

--- pc_network_monitor.ino
+++ pc_network_monitor.ino
@@ -7,28 +7,26 @@ void setup() {
   lcd.print("Internet Speed");
   delay(2000);
   lcd.clear();
-  Serial.begin(9600); // Start Serial communication
+  Serial.begin(9600); 
 }
 
 void loop() {
-  // Read data sent from Python script
+
   if (Serial.available() >= 3) {
     char data_type = Serial.read();
     String data_value = Serial.readStringUntil('\n');
 
-    // Convert data_value to float
     float speed = data_value.toFloat();
 
-    // Check the data type and display the speed accordingly
     if (data_type == 'T') {
       lcd.setCursor(0, 0);
       lcd.print("Tx: ");
-      lcd.print(speed, 2); // Display with 2 decimal places
+      lcd.print(speed, 2); 
       lcd.print(" Mbps    ");
     } else if (data_type == 'R') {
       lcd.setCursor(0, 1);
       lcd.print("Rx: ");
-      lcd.print(speed, 2); // Display with 2 decimal places
+      lcd.print(speed, 2);
       lcd.print(" Mbps    ");
     }
   }
